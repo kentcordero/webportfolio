2ea7ae2f8ab9f2ce5a772e5ad356f23dcda6b063
corderokentroidagoy <corderokentroidagoy@gmail.com>
2024-05-12 12:57:21 +0800

    Updated title

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".auto-type", {
+      strings: ["Front End", "Back End", "Full Stack"],
+      typeSpeed: 150,
+      backSpeed: 150,
+      loop: true
+    });
+  });
